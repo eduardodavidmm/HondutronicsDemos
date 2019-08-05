20fe6ccacf9b5a003da3f6a2877426e6e9c2fcd1
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2019-08-04 20:04:54 -0600

    Firebase google auth done

--- hondutronicsShop_vol2/src/components/header/header.component.jsx
+++ hondutronicsShop_vol2/src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/logo.svg'
+import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo"></Logo>
@@ -11,7 +12,8 @@ const Header = () => (
 
         <div className="options">
             <Link className="option" to="/shop">Tienda</Link>
-            <Link className="option" to="/shop">Acerca de Nosotros</Link>
+            <Link className="option" to="/shop">Contáctanos</Link>
+            { currentUser ? (<div className="option" onClick={() => auth.signOut()}>Cerrar Sesión</div>) : (<Link className="option" to="/signin">Iniciar Sesión</Link>) }
         </div>
     </div>
 )
