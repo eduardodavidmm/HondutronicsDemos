c34bb676a10b34abb61fd98b65752987edcb3c08
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2019-08-10 20:12:10 -0600

    add stripe

---
+++ hondutronicsShop_vol2/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_bWXzMRnIJUSUNrfbkghb2bBd00YwugMeJm';
+
+    const onToken = token => {
+        alert('Pago Realizado');
+    }
+
+    return (
+       <StripeCheckout
+       label = 'Pagar'
+       name = 'Hondutronics Shop'
+       billingAddress
+       shippingAddress
+       image = 'https://svgshare.com/i/EV9.svg'
+       description = {`Tu pago total es: $${price} `}
+       amount = {priceForStripe}
+       panelLabel = 'Pagar'
+       token = {onToken}
+       stripeKey = {publishableKey}
+       /> 
+    );
+};
+
+export default StripeCheckoutButton;
